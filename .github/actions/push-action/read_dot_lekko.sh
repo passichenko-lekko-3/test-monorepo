418bcd8ca1935bc699027c350302356a9a716d01
passichenko-lekko-3 <passichenko.lekko.3@gmail.com>
2024-07-24 16:22:36 -0700

    Update read_dot_lekko.sh

--- .github/actions/push-action/read_dot_lekko.sh
+++ .github/actions/push-action/read_dot_lekko.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
+echo ${GITHUB_WORKSPACE}/${PROJECT_PATH}
 cd ${GITHUB_WORKSPACE}/${PROJECT_PATH}
 DOT_LEKKO="$(lekko conf)"
+echo $DOT_LEKKO
 echo "repository="$(echo "$DOT_LEKKO" | jq '.repository' --raw-output)"" >> $GITHUB_OUTPUT
 echo "lekko_path="$(echo "$DOT_LEKKO" | jq '.lekkoPath' --raw-output)"" >> $GITHUB_OUTPUT
